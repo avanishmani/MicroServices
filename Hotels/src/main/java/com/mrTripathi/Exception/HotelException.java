5a8f2a706e4f062c447f52185c83b671ec701f07
avanishmani <avanishmanitripathi@gmail.com>
2023-04-20 22:59:47 +0530

    20 April

---
+++ Hotels/src/main/java/com/mrTripathi/Exception/HotelException.java
@@ -0,0 +1,11 @@
+package com.mrTripathi.Exception;
+
+public class HotelException extends Exception {
+	public HotelException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public HotelException(String msg) {
+		super(msg);
+	}
+}
