36dbed7287eb3360de2adb34e9dc8a35a5d7a343
avanishmani <avanishmanitripathi@gmail.com>
2023-04-20 23:52:31 +0530

    20 April

--- Hotels/src/main/java/com/mrTripathi/HotelsApplication.java
+++ Hotels/src/main/java/com/mrTripathi/HotelsApplication.java
@@ -3,7 +3,10 @@ package com.mrTripathi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
+@OpenAPIDefinition
 public class HotelsApplication {
 
 	public static void main(String[] args) {
