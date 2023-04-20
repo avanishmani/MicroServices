ce3f5d3b3c25e9f431392217c41801154abdd535
avanishmani <avanishmanitripathi@gmail.com>
2023-04-20 23:04:46 +0530

    20 April

--- Hotels/src/main/java/com/mrTripathi/Repository/HotelRepository.java
+++ Hotels/src/main/java/com/mrTripathi/Repository/HotelRepository.java
@@ -1,9 +1,10 @@
 package com.mrTripathi.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mrTripathi.model.Hotel;
-
+@Repository
 public interface HotelRepository extends JpaRepository<Hotel, Integer> {
 
 }
