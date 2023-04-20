526d54d22b6f9c06c154d20eef1140fd54e93b89
avanishmani <avanishmanitripathi@gmail.com>
2023-04-20 23:08:25 +0530

    20 April

---
+++ Hotels/src/main/java/com/mrTripathi/Service/HotelService.java
@@ -0,0 +1,14 @@
+package com.mrTripathi.Service;
+
+import java.util.List;
+
+import com.mrTripathi.Exception.HotelException;
+import com.mrTripathi.model.Hotel;
+
+public interface HotelService {
+	Hotel addHotel(Hotel hotel);
+
+	List<Hotel> getAllHotel();
+
+	Hotel getHotel(Integer id) throws HotelException;
+}
