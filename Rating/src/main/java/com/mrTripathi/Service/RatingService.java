58207d89c498d4bcc293fd2daee2eaa9489d4b69
avanishmani <avanishmanitripathi@gmail.com>
2023-04-25 01:05:42 +0530

    25 April

---
+++ Rating/src/main/java/com/mrTripathi/Service/RatingService.java
@@ -0,0 +1,16 @@
+package com.mrTripathi.Service;
+
+import java.util.List;
+
+import com.mrTripathi.Exception.RatingException;
+import com.mrTripathi.model.Rating;
+
+public interface RatingService {
+	Rating createRating(Rating r);
+
+	List<Rating> getRating() throws RatingException;
+
+	List<Rating> getRatingByUserID(Integer UID) throws RatingException;
+
+	List<Rating> getRatingByHotelID(Integer HID) throws RatingException;
+}
---
+++ Rating/src/main/java/com/mrTripathi/Service/RatingServiceImpl.java
@@ -0,0 +1,43 @@
+package com.mrTripathi.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mrTripathi.Exception.RatingException;
+import com.mrTripathi.Repository.RatingRepo;
+import com.mrTripathi.model.Rating;
+@Service
+public class RatingServiceImpl implements RatingService {
+	@Autowired
+	private RatingRepo rr;
+
+	@Override
+	public Rating createRating(Rating r) {
+		// TODO Auto-generated method stub
+		return rr.save(r);
+	}
+
+	@Override
+	public List<Rating> getRating() throws RatingException {
+		List<Rating> lr=rr.findAll();
+		if(lr.size()==0)throw new RatingException("No Any Rating is Avilable");
+		else return lr;
+	}
+
+	@Override
+	public List<Rating> getRatingByUserID(Integer UID) throws RatingException {
+		List<Rating> lr=rr.findByUserID(UID);
+		if(lr.size()==0)throw new RatingException("No Any Rating is Avilable with UserID"+ UID);
+		else return lr;
+	}
+
+	@Override
+	public List<Rating> getRatingByHotelID(Integer HID) throws RatingException {
+		List<Rating> lr=rr.findByHotelID(HID);
+		if(lr.size()==0)throw new RatingException("No Any Rating is Avilable with UserID"+ HID);
+		else return lr;
+	}
+
+}
