d286c22d8f9fc1986a841fc315d7a24244059761
avanishmani <avanishmanitripathi@gmail.com>
2023-05-01 23:37:10 +0530

    1 May

--- Rating/src/main/java/com/mrTripathi/RatingApplication.java
+++ Rating/src/main/java/com/mrTripathi/RatingApplication.java
@@ -2,11 +2,13 @@ package com.mrTripathi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @SpringBootApplication
 @OpenAPIDefinition
+@EnableDiscoveryClient
 public class RatingApplication {
 
 	public static void main(String[] args) {
