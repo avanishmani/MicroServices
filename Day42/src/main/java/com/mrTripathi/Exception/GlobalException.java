1d2d8a6fde6a34978566e85fcbd1b404c3181bd4
avanishmani <avanishmanitripathi@gmail.com>
2023-05-01 23:35:37 +0530

    1 May

--- Day42/src/main/java/com/mrTripathi/Exception/GlobalException.java
+++ Day42/src/main/java/com/mrTripathi/Exception/GlobalException.java
@@ -2,7 +2,6 @@ package com.mrTripathi.Exception;
 
 import java.time.LocalDateTime;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
