3655ad28b8aacf1773edd71d763da36dec7b414c
avanishmani <avanishmanitripathi@gmail.com>
2023-05-07 17:22:43 +0530

    7 May

---
+++ Gatways/src/test/java/com/mrTripathi/GatwaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrTripathi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatwaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
