bc4d52bf798a3c32e5987b8cce98ca2cfabe215d
avanishmani <avanishmanitripathi@gmail.com>
2023-05-08 23:35:39 +0530

    8 May

--- Gatways/src/main/java/com/mrTripathi/GatwaysApplication.java
+++ Gatways/src/main/java/com/mrTripathi/GatwaysApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+
+
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition
 public class GatwaysApplication {
 
 	public static void main(String[] args) {
